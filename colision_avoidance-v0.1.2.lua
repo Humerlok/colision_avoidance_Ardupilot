2a9507cf432bcc4c6e505d571ae447df0aee5c51
Humerlok <renanmandelo@yahoo.com.br>
2026-04-01 14:36:36 -0300

    v0.1.2 bug fix na gambiarra

--- colision_avoidance-v0.1.2.lua
+++ colision_avoidance-v0.1.2.lua
@@ -7,6 +7,8 @@ local threshold -- configure a distância de segurança em cm no parametro SCR_U
 local droneMode
 local armingState
 local velocity
+local breakMode = false
+local rtlMode = false
 
 --variáveis de tempo
 local colisionTime = nil
@@ -18,59 +20,57 @@ local break_interval = 10000 --10s
 
 
 function avoidance()
-    now = millis()
+    if distance < threshold then --verifica se a distância é menor que o threshold
+        if not colisionTime then --timer para filtrar leituras falsas
+            colisionTime = now
+        end
 
-    if distance ~= nil then          --verifica se há dados de lidar
-        if distance < threshold then --verifica se a distância é menor que o threshold
-            if not colisionTime then --timer para filtrar leituras falsas
-                colisionTime = now
+        if now - colisionTime >= 500 then                   --timer para filtrar leituras falsas
+            if breakMode == false and rtlMode == false then --verifica se o drone não está em modo break
+                vehicle:set_mode(17)                        -- change to break mode
+                gcs:send_text(4, "BREAK! - Obstacle detected")
+                last_break_time = now
+                breakMode = true
             end
 
-            if now - colisionTime >= 500 then --timer para filtrar leituras falsas
-                droneMode = vehicle:get_mode()
-
-                if droneMode ~= 17 then  --verifica se o drone não está em modo break
-                    vehicle:set_mode(17) -- change to break mode
-                    gcs:send_text(6, "BREAK! - Obstacle detected")
-                    last_break_time = now
-                end
-
-                if droneMode == 17 and now - last_break_time >= break_interval then --verifica se o drone está em modo break e muda para RTL depois de 10s
-                    vehicle:set_mode(6)
-                    gcs:send_text(6, "Returning to Home - Collision Avoidance")
-                end
-
-                if now - last_msg_time >= msg_interval then --timer para menssagem não floodar
-                    gcs:send_text(6, "Obstacle Distance: " .. tostring(distance))
-                    last_msg_time = now
-                end
+            if breakMode and now - last_break_time >= break_interval then --verifica se o drone está em modo break e muda para RTL depois de 10s
+                vehicle:set_mode(6)
+                gcs:send_text(6, "Returning to Home - Collision Avoidance")
+                breakMode = false
+                rtlMode = true
             end
-        elseif distance <= threshold + threshold / 2 and distance > threshold then
+
             if now - last_msg_time >= msg_interval then --timer para menssagem não floodar
-                gcs:send_text(6, "Obstacle close")
                 gcs:send_text(6, "Obstacle Distance: " .. tostring(distance))
                 last_msg_time = now
-                colisionTime = nil
             end
-        else
-            colisionTime = nil
         end
+    elseif distance <= threshold + threshold / 2 and distance > threshold then
+        if now - last_msg_time >= msg_interval then --timer para menssagem não floodar
+            gcs:send_text(6, "Obstacle close")
+            gcs:send_text(6, "Obstacle Distance: " .. tostring(distance))
+            last_msg_time = now
+            colisionTime = nil
+            breakMode = false
+            rtlMode = false
+        end
+    else
+        colisionTime = nil
+    end
+end
+
+function update()
+    now = millis()
+    threshold = param:get("SCR_USER1") or 0
+    distance = rangefinder:distance_cm_orient(0) -- 0 = Frente (Forward). Altere se for outra direção.
+
+    if distance ~= nil then                      --verifica se há dados de lidar
+        avoidance()
     else
         if now - last_msg_time >= msg_interval then --timer para menssagem não floodar
             gcs:send_text(6, "Caution: No lidar data for avoidance")
             last_msg_time = now
         end
-    end
-end
-
-function update()
-    threshold = param:get("SCR_USER1") or 0
-    distance = rangefinder:distance_cm_orient(0) -- 0 = Frente (Forward). Altere se for outra direção.
-    armingState = vehicle:get_arming_state()
-    velocity = ahrs:get_groundspeed()
-
-    if velocity and velocity > 1 and armingState == 1 then
-        avoidance()
     end
 
     return update, 250
